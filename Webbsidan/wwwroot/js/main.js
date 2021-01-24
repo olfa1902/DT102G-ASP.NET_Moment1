b08bea4c72cfa1fb6eab86d277600b543cdaaf4f
olive <olive@5CG6236643>
2021-01-24 16:07:49 +0100

    Add project files.

---
+++ Webbsidan/wwwroot/js/main.js
@@ -0,0 +1,4 @@
+﻿/*Javascript-kod som lägger in lite text i en sista paragraf i footern*/
+var text = "nationalencyklopedin";
+var textlängd = text.length;
+document.getElementById("jsparagraf").innerHTML = "Denna textparagraf är skapad med JavaScript och visar att nationalencyklopedin är ett ord på " + textlängd + " bokstäver";
